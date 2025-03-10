8c7683c58b1dff6fb27977b28ca9e6eb46d592bd
CPMThein1999 <theinaung2232@gmail.com>
2025-03-10 17:43:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from rich.text import Text
 from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
-from theincpm import TheinCPM
+from cpmayan import CPMAyan
 
 __CHANNEL_USERNAME__ = "cpmta11"
 __GROUP_USERNAME__   = "not available"
@@ -131,7 +131,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = TheinCPM(acc_access_key)
+        cpm = CPMAyan(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
